6a33126629ecf4ca76116e82f6a0d2986114982d
fkaaziebu <frederickaziebu1998@gmail.com>
2022-12-25 00:46:42 +0000

    Step 2
    Using messaging_service_sid

---
+++ basics/basics_02.py
@@ -0,0 +1,17 @@
+from twilio.rest import Client
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+phone_number = os.getenv("PHONE_NUMBER")
+account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+client = Client(account_sid, auth_token)
+
+message = Client.messages.create(
+    body="Hello world from twilio", messaging_service_sid="twilio number", to=phone_number
+)
+
+print("SMS has been sent")
+print(message)
